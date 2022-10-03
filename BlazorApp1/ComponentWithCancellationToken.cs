8347b622d9ad17de58d6998f362309eedfd6fa87
Daniel Coates <daniel@coatesy.co.uk>
2022-10-03 21:27:37 +0100

    initial commit

---
+++ BlazorApp1/ComponentWithCancellationToken.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1
+{
+    public abstract class ComponentWithCancellationToken : ComponentBase, IDisposable
+    {
+        private CancellationTokenSource? _cancellationTokenSource;
+
+        protected CancellationToken ComponentDetached => (_cancellationTokenSource ??= new()).Token;
+
+        public virtual void Dispose()
+        {
+            if (_cancellationTokenSource != null)
+            {
+                _cancellationTokenSource.Cancel();
+                _cancellationTokenSource.Dispose();
+                _cancellationTokenSource = null;
+            }
+        }
+    }
+}
